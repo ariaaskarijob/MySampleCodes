e317a4c7b79b664f3d34fd90d6c00031d361a56f
ariaaskarijob <aria.askari.job@gmail.com>
2018-02-03 20:59:59 -0800

    Update send_email.py
    Updating with comments

--- send_email.py
+++ send_email.py
@@ -1,13 +1,18 @@
+#how to send an email with attachment, and a subject line, using python!
+
+#importing the right libraries for sending email
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-email_user = 'ca3108718857@gmail.com'
-email_password = ''
-email_send = ''
+#Email,passowrd,details
+email_user = 'Your Email'
+email_password = 'Your Password'
+email_send = 'The Receiver Email'
 
+#Your Custome Subject line
 subject = 'Ice Cream Next Week?'
 
 msg = MIMEMultipart()
@@ -15,23 +20,26 @@ msg['From'] = email_user
 msg['To'] = email_send
 msg['Subject'] = subject
 
-body = 'This is just a test, but a really good achievement.'
+#Your body of the email
+body = 'You can write anything here, just like an email.'
 msg.attach(MIMEText(body,'plain'))
 
+#Attach-file, making sure the code runs within the same folder.
 filename='2.jpg'
 attachment  =open(filename,'rb')
 
+#encoding and standard steps
 part = MIMEBase('application','octet-stream')
 part.set_payload((attachment).read())
 encoders.encode_base64(part)
 part.add_header('Content-Disposition',"attachment; filename= "+filename)
-
 msg.attach(part)
 text3 = msg.as_string()
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
 server.login(email_user,email_password)
 
-
+#where the actual email gets send, and quits after
 server.sendmail(email_user,email_send,text3)
 server.quit()
+
